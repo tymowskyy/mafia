341e3596f78f79bcd3b2ca73d70216253ad9860a
tymowskyy <tymowskyy@gmail.com>
2025-01-02 15:31:36 +0100

    initial commit

---
+++ mafia/mafiaApp.swift
@@ -0,0 +1,17 @@
+//
+//  mafiaApp.swift
+//  mafia
+//
+//  Created by Tymoteusz Stępkowski on 02/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct mafiaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
